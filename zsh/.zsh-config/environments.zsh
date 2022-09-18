9654b5a6dc4774339e5b5900c512f0f6a2323fb4
akky-xxxx <114.s.aki@gmail.com>
2022-09-19 00:39:14 +0900

    add setopt hist_ignore_dups

--- zsh/.zsh-config/environments.zsh
+++ zsh/.zsh-config/environments.zsh
@@ -9,5 +9,6 @@ if [ -f '/Users/shogo/google-cloud-sdk/path.zsh.inc' ]; then . '/Users/shogo/goo
 # The next line enables shell command completion for gcloud.
 if [ -f '/Users/shogo/google-cloud-sdk/completion.zsh.inc' ]; then . '/Users/shogo/google-cloud-sdk/completion.zsh.inc'; fi
 
+setopt hist_ignore_dups
 autoload -U +X bashcompinit && bashcompinit
 complete -o nospace -C /usr/local/bin/terraform terraform
